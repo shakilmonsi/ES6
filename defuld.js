9d3963b38ac7be43e5844db7caba24321d018f33
shakilmonsi <samdshakilofficial@gmail.com>
2024-08-25 23:25:23 +0600

    Initial commit

---
+++ defuld.js
@@ -0,0 +1,25 @@
+function add(num1, num2){
+    const result = num1 + num1;
+    console.log(num1,num2,result)
+    return result;
+
+}
+const sum =add (21,12)
+
+const last = "pakhi "
+const greet = "pfdedfdakhi "
+const name = "pakdjudjjhdhi "
+const all = last + " "+  greet +" "+ name ;
+console.log(name)
+
+
+
+
+
+
+
+
+
+
+
+
